690550b6d688ffd9d43a61687a03e05cbaca4eec
Shinobu686 <choppu.business@gmail.com>
2020-10-12 20:54:22 +0900

    新規プロジェクト

---
+++ SamplePhoneBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SamplePhoneBook
+//
+//  Created by 久富稜也 on 2020/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
