3aabce07d53366ccda110dedf6e102c2a7470721
Shinobu686 <choppu.business@gmail.com>
2020-10-27 18:17:53 +0900

    fix #7 プロフィール一覧作成

---
+++ SamplePhoneBook/ProfileRegistration.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileRegistration.swift
+//  SamplePhoneBook
+//
+//  Created by 久富稜也 on 2020/10/22.
+//
+
+import SwiftUI
+
+struct ProfileRegistration: View {
+    var body: some View {
+        Text("プロフィール登録ページ")
+    }
+}
+
+struct ProfileRegistration_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileRegistration()
+    }
+}
